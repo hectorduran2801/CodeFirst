fe6444a265a006981de5ef87f7491ac2a08d47a2
hector <hduranc24@gmail.com>
2024-09-25 09:23:08 -0500

    agregar vista de tabla temporal

---
+++ CodeFirst/Controllers/LsTController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using DBCodeFirst;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LsTController : ControllerBase
+    {
+
+        private readonly CodeFirstContext _context;
+
+        public LsTController(CodeFirstContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ListadoTemporal>>> GetListados()
+        {
+            return await _context.ListadosTemporal.ToListAsync();
+        }
+
+    }
+}
